72c22b6b8551a92442570d3f13fd077b1d59ab3a
Alex Filipek <awfilipek@gmail.com>
2025-08-14 15:48:48 -0400

    fix error

---
+++ frontend/src/app/transactions/components/ClientTransactions.tsx
@@ -0,0 +1,169 @@
+"use client";
+
+import React, { useEffect, useState, useCallback } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useShallow } from "zustand/shallow";
+import Pagination from "@mui/material/Pagination";
+import TransactionSearchBar from "./TransactionSearchBar";
+import TransactionsTable from "./TransactionsTable";
+import ContentContainer from "../../../components/ContentContainer";
+import { useAppStore } from "@/stores/useAppStore";
+import { debounce } from "lodash";
+import { PaginationMetaData } from "@/types";
+
+const ClientTransactions = () => {
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const router = useRouter();
+
+  console.log("PAGE RERENDER: TRANSACTIONS PAGE");
+
+  const [paginationData, setPaginationData] = useState<PaginationMetaData>();
+
+  useEffect(() => {
+    setSearch(searchParams.get("search") || "");
+    setPage(Number(searchParams.get("page")) || 1);
+    setCategoryFilter(searchParams.get("category") || "");
+  }, [searchParams]);
+
+  const { budgets, fetchBudgets } = useAppStore(
+    useShallow((state) => ({
+      budgets: state.budgets,
+      fetchBudgets: state.fetchBudgets,
+    }))
+  );
+
+  const budgetNames = budgets.map((budget) => budget.name);
+
+  const { transactions, fetchTransactions, loading } = useAppStore(
+    useShallow((state) => ({
+      transactions: state.transactions,
+      fetchTransactions: state.fetchTransactions,
+      loading: state.loading,
+      error: state.error,
+    }))
+  );
+
+  // Debounce the fetchTransactions call itself
+  // const debouncedFetch = useCallback(
+  //   debounce((page: number, search: string, categoryFilter: string) => {
+  //     fetchTransactions({ page, search, categoryFilter: categoryFilter });
+  //   }, 300),
+  //   [fetchTransactions]
+  // );
+
+  // Debounced fetch for search only
+  const debouncedFetchBySearch = useCallback(
+    debounce((searchValue: string) => {
+      fetchTransactions({ page, search: searchValue, categoryFilter });
+    }, 300),
+    [fetchTransactions, page, categoryFilter]
+  );
+
+  useEffect(() => {
+    async function loadTransactions() {
+      const result = (await fetchTransactions({
+        page,
+        search,
+        categoryFilter,
+      })) as PaginationMetaData;
+      setPaginationData(result);
+    }
+    loadTransactions();
+    fetchBudgets();
+  }, []);
+
+  // // Trigger immediate fetch when page or categoryFilter changes
+  // useEffect(() => {
+  //   fetchBudgets();
+  // }, [fetchBudgets]);
+
+  // // Trigger immediate fetch when page or categoryFilter changes
+  // useEffect(() => {
+  //   async function loadTransactions() {
+  //     const result = (await fetchTransactions({
+  //       page,
+  //       search,
+  //       categoryFilter,
+  //     })) as PaginationMetaData;
+  //     setPaginationData(result);
+  //   }
+  //   loadTransactions();
+  // }, [page, categoryFilter, fetchTransactions]);
+
+  // Trigger debounced fetch when search changes
+  useEffect(() => {
+    console.log("USE EFFECT C");
+    debouncedFetchBySearch(search);
+    return debouncedFetchBySearch.cancel;
+  }, [search, debouncedFetchBySearch]);
+
+  useEffect(() => {
+    console.log("USE EFFECT D");
+    updateUrlParams({ search, page, category: categoryFilter });
+  }, [search, page, categoryFilter]);
+
+  // TODO: Any way to simplify or separate any of this logic
+  const updateUrlParams = (newParams: {
+    search?: string;
+    page?: number;
+    category?: string;
+  }) => {
+    const params = new URLSearchParams(window.location.search);
+
+    // Update with current state
+    if (newParams.search === "") {
+      params.delete("search");
+    } else {
+      params.set("search", search);
+    }
+
+    if (newParams.page === 1) {
+      params.delete("page");
+    } else {
+      params.set("page", page.toString());
+    }
+
+    if (newParams.category === "") {
+      params.delete("category");
+    } else {
+      params.set("category", categoryFilter);
+    }
+
+    router.push(`?${params.toString()}`);
+  };
+
+  return (
+    <ContentContainer title="Transactions">
+      <div className="bg-white p-6">
+        <TransactionSearchBar
+          onSearchChange={(search: string) => setSearch(search)}
+          onCategoryFilterChange={(category: string) =>
+            setCategoryFilter(category)
+          }
+          search={search}
+          budgetNames={budgetNames}
+        />
+        {loading && transactions.length === 0 ? (
+          <div>Loading</div>
+        ) : (
+          <TransactionsTable transactions={transactions} />
+        )}
+
+        <div className="flex items-center w-full justify-center mt-4">
+          <Pagination
+            count={paginationData?.lastPage || 1}
+            page={paginationData?.page || 1}
+            variant="outlined"
+            shape="rounded"
+            onChange={(e: React.ChangeEvent, page: number) => setPage(page)}
+          />
+        </div>
+      </div>
+    </ContentContainer>
+  );
+};
+
+export default ClientTransactions;
